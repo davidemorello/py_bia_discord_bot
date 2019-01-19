7f7b2e9e2220531a9bb1210ca30bc0404646841e
davidemorello <davide.morello@a-sign.it>
2019-01-19 13:48:52 +0100

    Add files via upload

---
+++ BIADiscordBOT/config/config.py
@@ -0,0 +1,8 @@
+config = {
+    'bot_token': 'mytoken',
+    'csvfile': '.../files/common.csv',
+    'pubg_file': '.../files/pubg_users.json',
+    'pubg_player_stats': '.../files/pubg_player_stats.json',
+    'pubg_token': 'mytoken',
+    'min_played_match': '10'
+    }
